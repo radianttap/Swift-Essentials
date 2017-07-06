8f5c9d9b30537adb810aaea6e4d7f8bf01ffb3a0
Aleksandar Vacic <av@radianttap.com>
2017-07-06 11:03:31 +0200

    Allow usage of this protocol for non-final classes
    Resolves: Protocol requirement cannot be satisfied by a non-final class because it uses 'Self' in a non-parameter, non-result type position

--- Views/NibLoadable.swift
+++ Views/NibLoadable.swift
@@ -16,9 +16,6 @@ protocol NibLoadableView {
 
 	///	Instantiates UINib using `nibName` as the name, from the main bundle
 	static var nib: UINib { get }
-
-	///	Creates an instance of the cell from the `nibName`.xib file
-	static var nibInstance : Self { get }
 }
 
 extension NibLoadableView where Self: UIView {
@@ -29,7 +26,20 @@ extension NibLoadableView where Self: UIView {
 	static var nib: UINib {
 		return UINib(nibName: self.nibName, bundle: nil)
 	}
+}
 
+protocol NibReusableView : ReusableView, NibLoadableView {}
+
+
+
+///	Adopt this in cases where you need to create an ad-hoc instance of the given view
+///	Can be adopted only by classes marked as `final`, due to `Self` constraint
+protocol NibLoadableFinalView: NibLoadableView {
+	///	Creates an instance of the cell from the `nibName`.xib file
+	static var nibInstance : Self { get }
+}
+
+extension NibLoadableFinalView {
 	static var nibInstance : Self {
 		guard let nibObject = self.nib.instantiate(withOwner: nil, options: nil).last as? Self else {
 			fatalError("Failed to create an instance of \(self) from \(self.nibName) nib.")
@@ -38,5 +48,3 @@ extension NibLoadableView where Self: UIView {
 	}
 }
 
-
-protocol NibReusableView : ReusableView, NibLoadableView {}
