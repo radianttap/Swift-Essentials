f21200a084952e8dcf904eefd83486341264533b
Aleksandar Vacic <av@radianttap.com>
2017-08-17 22:22:30 +0200

    Added support for UICV supplementaries and UITV header/footer

--- Views/DequeableView.swift
+++ Views/DequeableView.swift
@@ -19,8 +19,8 @@ extension UICollectionView {
 	}
 
 	//	register for the Nib-based cell
-	func register<T: UICollectionViewCell>(_: T.Type) where
-		T:NibReusableView
+	func register<T: UICollectionViewCell>(_: T.Type)
+		where T:NibReusableView
 	{
 		register(T.nib, forCellWithReuseIdentifier: T.reuseIdentifier)
 	}
@@ -34,6 +34,31 @@ extension UICollectionView {
 			fatalError("Dequeing a cell with identifier: \(T.reuseIdentifier) failed.\nDid you maybe forget to register it in viewDidLoad?")
 		}
 		return cell
+	}
+
+	//	register for the Class-based supplementary view
+	func register<T: UICollectionReusableView>(_: T.Type, kind: String)
+		where T:ReusableView
+	{
+		register(T.self, forSupplementaryViewOfKind: kind, withReuseIdentifier: T.reuseIdentifier)
+	}
+
+	//	register for the Nib-based supplementary view
+	func register<T: UICollectionReusableView>(_: T.Type, kind: String)
+		where T:NibReusableView
+	{
+		register(T.nib, forSupplementaryViewOfKind: kind, withReuseIdentifier: T.reuseIdentifier)
+	}
+
+	func dequeueReusableView<T: UICollectionReusableView>(kind: String, atIndexPath indexPath: IndexPath) -> T
+		where T:ReusableView
+	{
+		//	this deque and cast can fail if you forget to register the proper cell
+		guard let view = dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+			//	thus crash instantly and nudge the developer
+			fatalError("Dequeing supplementary view of kind: \( kind ) with identifier: \( T.reuseIdentifier ) failed.\nDid you maybe forget to register it in viewDidLoad?")
+		}
+		return view
 	}
 }
 
@@ -61,6 +86,27 @@ extension UITableView {
 			fatalError("Dequeing a cell with identifier: \(T.reuseIdentifier) failed.\nDid you maybe forget to register it in viewDidLoad?")
 		}
 		return cell
+	}
+
+	//	register for the Class-based header/footer view
+	func register<T: UITableViewHeaderFooterView>(_: T.Type)
+		where T:ReusableView
+	{
+		register(T.self, forHeaderFooterViewReuseIdentifier: T.reuseIdentifier)
+	}
+
+	//	register for the Nib-based header/footer view
+	func register<T: UITableViewHeaderFooterView>(_: T.Type)
+		where T:NibReusableView
+	{
+		register(T.self, forHeaderFooterViewReuseIdentifier: T.reuseIdentifier)
+	}
+
+	func dequeueReusableView<T: UITableViewHeaderFooterView>() -> T?
+		where T:ReusableView
+	{
+		let v = dequeueReusableHeaderFooterView(withIdentifier: T.reuseIdentifier) as? T
+		return v
 	}
 }
 
