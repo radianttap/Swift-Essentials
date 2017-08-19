103d3f8e430e441cc4c796b0f10d8be43f58b26a
Aleksandar Vacic <av@radianttap.com>
2017-08-19 14:59:38 +0200

    Added dummy demo project

---
+++ Demo/Demo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Demo
+//
+//  Created by Aleksandar Vacić on 19.8.17..
+//  Copyright © 2017. Radiant Tap. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		return true
+	}
+}
+
---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Aleksandar Vacić on 19.8.17..
+//  Copyright © 2017. Radiant Tap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
+
