d54f23da4441e1f08ca0f886ee5a38508ae53a65
Aleksandar Vacic <av@radianttap.com>
2017-08-19 20:40:51 +0200

    Make life easier for people who want to use this a framework

--- Operation/AsyncOperation.swift
+++ Operation/AsyncOperation.swift
@@ -8,8 +8,8 @@
 
 import Foundation
 
-class AsyncOperation : Operation {
-	enum State {
+open class AsyncOperation : Operation {
+	public enum State {
 		case ready
 		case executing
 		case finished
@@ -37,19 +37,19 @@ class AsyncOperation : Operation {
 		}
 	}
 
-	final override var isAsynchronous: Bool {
+	final override public var isAsynchronous: Bool {
 		return true
 	}
 
-	final override var isExecuting: Bool {
+	final override public var isExecuting: Bool {
 		return state == .executing
 	}
 
-	final override var isFinished: Bool {
+	final override public var isFinished: Bool {
 		return state == .finished
 	}
 
-	final override var isReady: Bool {
+	final override public var isReady: Bool {
 		return state == .ready
 	}
 
@@ -68,12 +68,12 @@ class AsyncOperation : Operation {
 		markFinished()
 	}
 
-	required override init() {
+	required override public init() {
 	}
 
 	//MARK: Control
 
-	final override func start() {
+	final override public func start() {
 		if isCancelled {
 			state = .finished
 			return
@@ -82,7 +82,7 @@ class AsyncOperation : Operation {
 		main()
 	}
 
-	final override func main() {
+	final override public func main() {
 		if isCancelled {
 			state = .finished
 			return
