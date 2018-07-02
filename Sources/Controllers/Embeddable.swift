4304901d5c6ba5ef75e730ec82c48a4640af19a1
Aleksandar Vacic <av@codeaplus.com>
2018-07-02 12:49:11 +0200

    Re-organized files and folders

--- Sources/Controllers/Embeddable.swift
+++ Sources/Controllers/Embeddable.swift
@@ -5,19 +5,35 @@
 //
 //  Copyright © 2016 Radiant Tap
 //  MIT License · http://choosealicense.com/licenses/mit/
-//
-//	Uses: https://github.com/roberthein/TinyConstraints
 
 import UIKit
-import TinyConstraints
 
 extension UIViewController {
 	///	(view, parentView) -> Void
 	public typealias LayoutBlock = (UIView, UIView) -> Void
 
+	///	Embeds the `view` of the given UIViewController into supplied `parentView` (or into `self.view`, if `nil`).
+	///
+	///	Default value of `LayoutBlock` aligns the embedded `view` with the edges of the `parentView`, using priority=999 for the bottom and trailing constraints).
+	///	This helps to avoid auto-layout issues when embedding into zero-width or height container views.
 	public func embed<T>(controller vc: T, into parentView: UIView?, layout: LayoutBlock = {
 		v, pv in
-		v.edges(to: pv, priority: LayoutPriority(999))
+
+		let constraints: [NSLayoutConstraint] = [
+			v.topAnchor.constraint(equalTo: pv.topAnchor),
+			v.leadingAnchor.constraint(equalTo: pv.leadingAnchor),
+			{
+				let lc = v.bottomAnchor.constraint(equalTo: pv.topAnchor)
+				lc.priority = UILayoutPriority(rawValue: 999)
+				return lc
+			}(),
+			{
+				let lc = v.trailingAnchor.constraint(equalTo: pv.trailingAnchor)
+				lc.priority = UILayoutPriority(rawValue: 999)
+				return lc
+			}()
+		]
+		constraints.forEach { $0.isActive = true }
 	})
 		where T: UIViewController
 	{
