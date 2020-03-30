3ffc3c400bb9ed66ab02824988f703e94f42f06c
Aleksandar Vacić <av@radianttap.com>
2020-03-30 12:19:44 +0200

    Completely re-factored internal working.
    Simplified it greatly, overriding just the minimum of stuff. Ran through 10min of TSan testing on large real project with no threading issues reported.

--- Sources/Operation/AsyncOperation.swift
+++ Sources/Operation/AsyncOperation.swift
@@ -10,110 +10,52 @@
 import Foundation
 
 open class AsyncOperation : Operation {
-	private enum State {
-		case ready
-		case executing
-		case finished
-
-		fileprivate var key: String {
-			switch self {
-			case .ready:
-				return "isReady"
-			case .executing:
-				return "isExecuting"
-			case .finished:
-				return "isFinished"
-			}
-		}
-	}
-
-	private let queue = DispatchQueue(label: "com.radianttap.Essentials.AsyncOperation")
-
-	/// Private backing store for `state`
-	private var _state: Atomic<State> = Atomic(.ready)
-
-	/// The state of the operation
-	private var state: State {
-		get {
-			return _state.atomic
-		}
-		set {
-			// A state mutation should be a single atomic transaction. We can't simply perform
-			// everything on the isolation queue for `_state` because the KVO willChange/didChange
-			// notifications have to be sent from outside the isolation queue. Otherwise we would
-			// deadlock because KVO observers will in turn try to read `state` (by calling
-			// `isReady`, `isExecuting`, `isFinished`. Use a second queue to wrap the entire
-			// transaction.
-			queue.sync {
-				// Retrieve the existing value first. Necessary for sending fine-grained KVO
-				// willChange/didChange notifications only for the key paths that actually change.
-				let oldValue = _state.atomic
-				guard newValue != oldValue else {
-					return
-				}
-				willChangeValue(forKey: oldValue.key)
-				willChangeValue(forKey: newValue.key)
-				_state.mutate {
-					$0 = newValue
-				}
-				didChangeValue(forKey: oldValue.key)
-				didChangeValue(forKey: newValue.key)
-			}
-		}
-	}
+	private var hasAsyncFinished = false
 
 	final override public var isAsynchronous: Bool {
 		return true
 	}
-
-	final override public var isExecuting: Bool {
-		return state == .executing
-	}
-
+	
 	final override public var isFinished: Bool {
-		return state == .finished
+		return hasAsyncFinished
 	}
-
-	final override public var isReady: Bool {
-		return state == .ready
+	
+	final override public var isExecuting: Bool {
+		return !hasAsyncFinished
 	}
-
 
 	//MARK: Setup
 
-	///	Do not override this method, ever. Call it from `workItem()` instead
+	///	Call this method from `workItem()`.
 	final public func markFinished() {
-		state = .finished
+		willChangeValue(forKey: "isExecuting")
+		willChangeValue(forKey: "isFinished")
+		hasAsyncFinished = true
+		didChangeValue(forKey: "isExecuting")
+		didChangeValue(forKey: "isFinished")
 	}
 
-	/// You **should** override this method and start and/or do your async work here.
-	///	**Must** call `markFinished()` inside your override
-	///	when async work is done since operation needs to be mark `finished`.
+	/// Override this method and perform your async work here.
+	///
+	///	**Must** call `markFinished()` inside your override when async work is done in order to mark the operation as `finished`.
 	open func workItem() {
 		markFinished()
 	}
 
-	required override public init() {
-	}
+	required override public init() {}
 
 	//MARK: Control
-
-	final override public func start() {
-		if isCancelled {
-			state = .finished
-			return
-		}
-
-		main()
+	
+	open override func cancel() {
+		hasAsyncFinished = true
+		super.cancel()
 	}
-
+	
 	final override public func main() {
-		if isCancelled {
-			state = .finished
+		if hasAsyncFinished {
 			return
 		}
 
-		state = .executing
 		workItem()
 	}
 }
